f5fa58dead0904648c36a7fa00311e0de4a39985
Deldrel <plapolla9@gmail.com>
2024-10-11 09:13:28 +0200

    feat: some refactoring

--- main.py
+++ main.py
@@ -6,23 +6,23 @@ from file_manager import file_filter, file_scan
 
 
 def main():
-    config = load_config(Path('config.yaml'))
-
-    files = file_scan(config['search_directories'])
-
-    files = file_filter(files, config['allowed_extensions'])
-
-    duplicates = find_duplicates(files, config['chunk_size'])
-
-    save_results(duplicates)
-
-
-if __name__ == "__main__":
     try:
-        main()
+        config = load_config(Path('config.yaml'))
+
+        files = file_scan(config['search_directories'])
+
+        files = file_filter(files, config['allowed_extensions'])
+
+        duplicates = find_duplicates(files, config['chunk_size'])
+
+        save_results(duplicates)
     except KeyboardInterrupt:
         print("Execution interrupted by the user.")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
     finally:
         input("Press Enter to exit...")
+
+
+if __name__ == "__main__":
+    main()
